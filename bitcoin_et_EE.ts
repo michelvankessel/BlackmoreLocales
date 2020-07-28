c96b25376b9031dfeefae6edb7a3a1616b480581
Michel van Kessel <michelvankessel@protonmail.com>
2020-07-28 20:44:21 +0200

    Estonian (Estonia)

--- bitcoin_et_EE.ts
+++ bitcoin_et_EE.ts
@@ -266,7 +266,7 @@
     </message>
     <message>
         <source>Bitcoin</source>
-        <translation>Bitcoin</translation>
+        <translation>Blackcoin</translation>
     </message>
     <message>
         <source>Error</source>
